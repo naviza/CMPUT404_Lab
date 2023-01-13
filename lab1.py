86b49088d02d6e3ad7a6514480c884e8c33bef3e
Richmond Naviza <naviza@ualberta.ca>
2023-01-13 12:35:27 -0700

    Update for downloading

--- lab1.py
+++ lab1.py
@@ -4,3 +4,7 @@ print(requests.__version__)
 
 r = requests.get("http://www.google.com")
 print(r)
+
+
+r = requests.get("https://raw.githubusercontent.com/naviza/CMPUT404_Lab/main/lab1.py", allow_redirects=True)
+open('lab1.py', 'wb').write(r.content)
